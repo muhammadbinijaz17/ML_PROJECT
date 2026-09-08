e435bbaa4d9526a311c07764d3a000dbbbf65b76
MUHAMMAD BIN IJAZ <muhammadbinijaz17@gmail.com>
2026-09-08 19:32:21 +0500

    HyperParameter Tunning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -40,17 +40,47 @@ class ModelTrainer:
                 test_array[:, -1]
             )
             models = {
-                "Random Forest": RandomForestRegressor(),
-                "Decision Tree": DecisionTreeRegressor(),
-                "Gradient Boosting": GradientBoostingRegressor(),
-                "Linear Regression": LinearRegression(),
-                "K-Neighbors Regressor": KNeighborsRegressor(),
-                "XGBRegressor": XGBRegressor(),
-                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                "AdaBoost Regressor": AdaBoostRegressor(),
-                }
+            "Random Forest": RandomForestRegressor(),
+            "Decision Tree": DecisionTreeRegressor(),
+            "Gradient Boosting": GradientBoostingRegressor(),
+            "Linear Regression": LinearRegression(),
+            "K-Neighbors Regressor": KNeighborsRegressor(),
+            "XGBRegressor": XGBRegressor(),
+            "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+            "AdaBoost Regressor": AdaBoostRegressor()
+            }
+        
+            params = {
+            "Decision Tree": {
+                'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+            },
+            "Random Forest": {
+                'n_estimators': [8, 16, 32, 64, 128, 256]
+            },
+            "Gradient Boosting": {
+                'learning_rate': [.1, .01, .05, .001],
+                'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                'n_estimators': [8, 16, 32, 64, 128, 256]
+            },
+            "Linear Regression": {},
+            "K-Neighbors Regressor": {},
+            "XGBRegressor": {
+                'learning_rate': [.1, .01, .05, .001],
+                'n_estimators': [8, 16, 32, 64, 128, 256]
+            },
+            "CatBoosting Regressor": {
+                'depth': [6, 8, 10],
+                'learning_rate': [.1, .01, .05, .001],
+                'iterations': [30, 50, 100]
+            },
+            "AdaBoost Regressor": {
+                'learning_rate': [.1, .01, 0.5, .001],
+                'n_estimators': [8, 16, 32, 64, 128, 256]
+            }
+            }
 
-            model_report: dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+
+            model_report: dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models,params=params)
 
             best_model_score = max(sorted(model_report.values()))
 
